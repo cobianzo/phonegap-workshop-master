de5d6a047d61f06691a6827e575d8219fa57a765
Alvaro B <cobianzo@gmail.com>
2015-04-30 00:45:08 +0200

    Part 3: Using Native Notification

--- js/main.js
+++ js/main.js
@@ -14,9 +14,21 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+    	self		= this;
+        this.store	= new MemoryStore( function(){self.showAlert("Se ha inicializado MemoryStore", "Información");} );
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
+
+
+
+	// esto es para usar el diálog de alertas nativo cuando corremos la app, y del browser cdo en browser. 
+	showAlert: function (message, title) {
+    	if (navigator.notification) 
+        	navigator.notification.alert(message, null, title, 'OK');  // objeto nativo del móvil
+	    else 
+    	    alert(title ? (title + ": " + message) : message); // browser
+	},
+
 
 };
 
