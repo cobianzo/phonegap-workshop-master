94b7c5f8f8933b3263f9bc4fb998429e96b023aa
Alvaro B <cobianzo@gmail.com>
2015-04-30 18:05:36 +0200

    Part 6. Including Templates via handlebars.js

--- js/main.js
+++ js/main.js
@@ -2,19 +2,46 @@ var app = {
 
     findByName: function() {
         console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
+        this.store.findByName(	$('.search-key').val(), 
+        		function(employees) {
+		            $('.employee-list').html(self.employeeLiTpl(employees));  // Tpl declared in index.hmtl
+        		});
     },
 
+
+	/*  VIEWS :  HTML */
+	 renderHomeView: function() {
+		 $("body").html(
+					 		this.homeTpl()
+		 );
+         $('.search-key').on('keyup', $.proxy(this.findByName, this));	 	
+	},
+
+
+	// para usar el diálog de alertas nativo cdo corremos la app, y del browser cdo en browser. 
+	showAlert: function (message, title) {
+    	if (navigator.notification) 
+        	navigator.notification.alert(message, null, title, 'OK');  // objeto nativo del móvil
+	    else 
+    	    alert(title ? (title + ": " + message) : message); // browser
+	},
+	
+
+
+
+	
+	/*
+	###########	###########	###########	###########	###########	###########
+	###########			INITIALIZE FUNCTION FOR THE APP			###########	
+	###########	###########	###########	###########	###########	###########		*/
+	
     initialize: function() {
-    	self		= this;
+    	
+    	/* Hndlebars: load templates created in index.html (but could be in external files) */
+		this.homeTpl 		= Handlebars.compile($("#home-tpl").html());
+		this.employeeLiTpl 	= Handlebars.compile($("#employee-li-tpl").html());    	
+
+    	self		= this;		
         this.store	= new MemoryStore( 
         	
         	function(){
@@ -23,35 +50,9 @@ var app = {
         
     },
 
-
-	/*  VIEWS :  HTML */
-	 renderHomeView: function() {
-	 $("body").html(
-			"<div class='header'><h1>Home</h1></div>" +
-            "<div class='search-view'>" +
-            "<input class='search-key'/>" +
-            "<ul class='employee-list'></ul>" +
-            "</div>"	 
-	 );
-	 	
-	},
+/*	###########	###########	###########	###########	###########	###########		*/
 
 
-
-
-
-
-
-
-	// esto es para usar el diálog de alertas nativo cuando corremos la app, y del browser cdo en browser. 
-	showAlert: function (message, title) {
-    	if (navigator.notification) 
-        	navigator.notification.alert(message, null, title, 'OK');  // objeto nativo del móvil
-	    else 
-    	    alert(title ? (title + ": " + message) : message); // browser
-	},
-	
-	
 	
 
 
