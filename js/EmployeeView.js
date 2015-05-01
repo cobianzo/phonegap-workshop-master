9bc92ff4edf4c9456d83834420aae6c4b738b5e1
Alvaro B <cobianzo@gmail.com>
2015-05-01 15:42:36 +0200

    Part 11. Save Contact into phone contact's

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -2,8 +2,12 @@
 function EmployeeView(employeeData){		// called with EmployeeView()
 
 	this.init		= function(){
+		/* definiticón de ppdes privadas */
 		this.jViewContainer	= $("<div class='employee-details'></div>");	/* "this.jViewContainer" es necesario declararlo aquí para poder usarlo en addLocation y aplicarle en onclick*/
+		
+		/* definiticón de métodos */		
 		this.jViewContainer.on('click', '.add-location-btn', this.addLocation);
+		this.jViewContainer.on('click', '.add-contact-btn', this.addToContacts);		
 	};
 
 	// called on click so event.target should give the calling dom element
@@ -23,6 +27,29 @@ function EmployeeView(employeeData){		// called with EmployeeView()
 			});
 		return false;
 	};	
+	
+	this.addToContacts = function(event) {
+		event.preventDefault();
+		console.log('addToContacts');
+		if (!navigator.contacts) {
+			app.showAlert("Contacts API not supported", "Error");
+			return;
+		}
+		var contact 			= navigator.contacts.create();			// <--  NAVIGATOR.contacts: objeto de ??? informarme! 
+		contact.name 			= {givenName: employee.firstName, familyName: employee.lastName};
+		contact.phoneNumbers	= [	new ContactField('work', employee.officePhone, false),		// <-- ContactField: declared where? 
+									new ContactField('mobile', employee.cellPhone, true) ]; 	// preferred number (true)
+		 	
+		contact.save();	// Salvado a contactos del móvil  !!!
+		return false;
+	};
+
+
+
+
+
+
+
 	
 	
 	
--- js/main.js
+++ js/main.js
@@ -7,36 +7,25 @@ var app = {
 	// para usar el diálog de alertas nativo cdo corremos la app, y del browser cdo en browser. 
 	showAlert: function (message, title) {
     	if (navigator.notification) 
-        	navigator.notification.alert(message, null, title, 'OK');  // objeto nativo del móvil
+        	navigator.notification.alert(message, null, title, 'OK');  	// objeto nativo del móvil
 	    else 
-    	    alert(title ? (title + ": " + message) : message); // browser
+    	    alert(title ? (title + ": " + message) : message); 			// browser
 	},
 	
 	registerEvents: function() {
 		var self = this;
+		
 		// Check of browser supports touch events...
-		if (document.documentElement.hasOwnProperty('ontouchstart')) {
-			// ... if yes: register touch event listener to change the "selected" state of the item
-			$('body').on('touchstart', 'a', function(event) {
-				$(event.target).addClass('tappable-active');
-			});
-			$('body').on('touchend', 'a', function(event) {
-				$(event.target).removeClass('tappable-active');
-			});
-		} else {
-			// ... if not: register mouse events instead
-			$('body').on('mousedown', 'a', function(event) {
-				$(event.target).addClass('tappable-active');
-				//self.showAlert("fds", "fdsds");
-			});
-			$('body').on('mouseup', 'a', function(event) {
-				$("a").removeClass('tappable-active');
-			});
+		if (document.documentElement.hasOwnProperty('ontouchstart')) {	// 	::: if yes: register touch event listener to change the "selected" state of the item
+			$('body').on('touchstart', 'a', function(event) {	$(event.target).addClass('tappable-active');		});
+			$('body').on('touchend', 'a', function(event) {		$(event.target).removeClass('tappable-active');		});
+		} else {// 															::: if not: register mouse events instead
+			$('body').on('mousedown', 'a', function(event) {	$(event.target).addClass('tappable-active');		});
+			$('body').on('mouseup', 'a', function(event) {		$("a").removeClass('tappable-active');				});
 		};
 		
-		// cada vez que cambie la url 'hashchange', llamamos a route, que recarga la página dinámicamente
-		$(window).on('hashchange', $.proxy(this.route, this));
-		
+		$(window).on('hashchange', $.proxy(this.route, this));					// ::: cada vez que cambie la url 'hashchange', llamamos a route, que recarga la página dinámicamente
+				
 	},
 	
 	
